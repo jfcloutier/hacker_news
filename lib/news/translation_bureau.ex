980878830aa3fa7131c5bfa850221f405b71b6e6
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2015-02-21 18:09:49 -0500

    Simplified translation request

--- lib/news/translation_bureau.ex
+++ lib/news/translation_bureau.ex
@@ -9,6 +9,7 @@ A translation bureau that relies on a network of translators.
 
 	@name __MODULE__
 	@group :translator
+  @key_file "google_apis.key"
 
 	def start_link( ) do
 		GenServer.start_link(@name, [], [name: @name])
@@ -18,41 +19,49 @@ A translation bureau that relies on a network of translators.
 		:pg2.start
 		:pg2.create(@group)
 		:pg2.join(@group, self)
-		{:ok, []}
+		{:ok, %{key: nil}}
   end
 
-	def translate(text, from, to) do
-		GenServer.call(@name, {:translate, text, from, to})
+	def translate(text, to) do
+		GenServer.call(@name, {:translate, text, to})
   end
 
-	def handle_call({:translate, text, from, to}, caller, state) do
+	def handle_call({:translate, text, to}, caller, state) do
+		{:ok, key} = get_key(state)
 		spawn( fn ->
-						 result = request_translation(text, from, to)
+						 result = request_translation(text, to, key)
 						 GenServer.reply(caller, result)
 					 end )
-		{:noreply, state}
+		{:noreply, %{state|key: key}}
   end
 
-	defp request_translation(text, from, to) do
+
+	defp request_translation(text, to, key) do
 		result = find_available_translator()
 		case result do
 			{:ok, pid} ->
-				{:ok, translation} = GenServer.call(pid, {:translate, text, from, to})
-				{:translation, translation, pid}
+				GenServer.call(pid, {:translate, text, to, key})
 		  {:error, message} ->
 				Logger.error(message)
 				{:error, message}
     end
   end
 
-  def find_available_translator() do
-		translators = :pg2.get_members(@group) 
-    |> Enum.filter(fn(pid) -> pid != self end)
+  defp find_available_translator() do
+		translators = :pg2.get_members(@group) |> Enum.filter(fn(pid) -> pid != self end)
 		case Enum.count(translators) do
-			 0 -> {:error, "No translator available"}
-			 n -> pid = Enum.at(translators, :random.uniform(n) - 1)
-            {:ok, pid}
+			0 -> {:error, "No translator available"}
+			n -> pid = Enum.at(translators, :random.uniform(n) - 1)
+           {:ok, pid}
     end
+  end
+
+  defp get_key(%{key: nil} = state) do
+		Logger.debug("Retrieving API key from file")
+		 File.read(@key_file)
+  end
+	defp get_key(%{key: key} = state) do
+		{:ok, key}
   end
 
 end
